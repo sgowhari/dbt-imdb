768ebd83253528a555716f41fbe2f8f259ad26ac
Shadi gowhari <sgowhari15@gmail.com>
2023-12-12 02:21:41 +0000

    restructuring folders

---
+++ models/staging/imdb/stg_imdb__crew_directors.sql
@@ -0,0 +1,6 @@
+SELECT 
+    tconst as title_id,  
+    directors as director_id_array--,
+   -- {{dbt_utils.generate_surrogate_key('title_id','director_id')}} as title_director_id
+
+FROM {{ ref('base_imdb__crew') }} as base_imdb__crew
