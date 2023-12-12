ba9412abddf97192ce01a8674deaccda8bc62aca
Shadi gowhari <sgowhari15@gmail.com>
2023-12-12 02:42:25 +0000

    added profiles.yml

--- models/staging/imdb/stg_imdb__crew_directors.sql
+++ models/staging/imdb/stg_imdb__crew_directors.sql
@@ -1,6 +1,5 @@
 SELECT 
-    tconst as title_id,  
-    directors as director_id_array--,
-   -- {{dbt_utils.generate_surrogate_key('title_id','director_id')}} as title_director_id
+    title_id,  
+    director_id_array
 
 FROM {{ ref('base_imdb__crew') }} as base_imdb__crew
