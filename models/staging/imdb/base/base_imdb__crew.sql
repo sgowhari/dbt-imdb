cc3071ad9f7197e3c23beafd4d329a2361bcad2b
Shadi gowhari <sgowhari15@gmail.com>
2023-12-13 17:55:53 +0000

    finalize base crew model

--- models/staging/imdb/base/base_imdb__crew.sql
+++ models/staging/imdb/base/base_imdb__crew.sql
@@ -1,6 +1,15 @@
 SELECT 
     tconst as title_id,  
-    directors as director_id_array,
-    writers as writer_id_array
+    CASE 
+        WHEN directors = 'NA' 
+            THEN NULL
+        ELSE SPLIT(directors, ',')
+    END as director_id_array,
+    CASE 
+        WHEN writers = 'NA'
+            THEN NULL
+        ELSE SPLIT(writers, ',')
+    END as writer_id_array
 
 FROM {{ source('imdb', 'title_crew') }} as title_crew
+WHERE 1=1
