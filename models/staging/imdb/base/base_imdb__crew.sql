5d6648b4d2659154c925f2d6c260d8821f5bb6f2
Shadi gowhari <sgowhari15@gmail.com>
2024-01-14 22:57:29 +0000

    updating project yaml

--- models/staging/imdb/base/base_imdb__crew.sql
+++ models/staging/imdb/base/base_imdb__crew.sql
@@ -1,15 +1,20 @@
-SELECT 
-    tconst as title_id,  
-    CASE 
-        WHEN directors = 'NA' 
-            THEN NULL
-        ELSE SPLIT(directors, ',')
-    END as director_id_array,
-    CASE 
-        WHEN writers = 'NA'
-            THEN NULL
-        ELSE SPLIT(writers, ',')
-    END as writer_id_array
+WITH title_crew AS 
+(
+    SELECT 
+        tconst as title_id,  
+        CASE 
+            WHEN directors = 'NA' 
+                THEN NULL
+            ELSE SPLIT(directors, ',')
+        END as director_id_array,
+        CASE 
+            WHEN writers = 'NA'
+                THEN NULL
+            ELSE SPLIT(writers, ',')
+        END as writer_id_array
 
-FROM {{ source('imdb', 'title_crew') }} as title_crew
-WHERE 1=1
+    FROM {{ source('imdb', 'title_crew') }} as title_crew
+)
+
+SELECT *
+FROM title_crew
